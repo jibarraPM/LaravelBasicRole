b65638140cb663f20c5c500b8186ed4bc9277a00
javier <jibarra@proyectomapache.cl>
2021-08-12 10:51:32 -0400

    Laravel mas roles

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('Sterek64'),
+            'role' => 1
+        ]);
+        User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('Sterek64'),
+            'role' => 2
+        ]);
+    }
+}
